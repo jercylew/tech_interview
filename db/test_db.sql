377bb75b99c45053c034b1dffa065637610b2c4d
Jercy Liu <jercy_lew@yahoo.com>
2018-11-04 18:40:53 +1100

    Improve the code of query category products

--- db/test_db.sql
+++ db/test_db.sql
@@ -2,31 +2,31 @@ use product_catalogue
 
 -- insert test data
 if not exists (
-		select * from catelogue
+		select * from Catelogue
 )
 begin
-	insert into catelogue values (1000, 'books', null)
-	insert into catelogue values (1001, 'philosophy', 1000)
-	insert into catelogue values (1002, 'metaphysics', 1001)
-	insert into catelogue values (1003, 'confucianism', 1001)
-	insert into catelogue values (1004, 'mencius', 1003)
-	insert into catelogue values (1005, 'literature', 1000)
-	insert into catelogue values (1006, 'lin yutang', 1005)
-	insert into catelogue values (1007, 'software', null)
-	insert into catelogue values (1008, 'utilities', 1007)
-	insert into catelogue values (1009, 'file management', 1008)
+	insert into Catelogue values (1000, 'books', null)
+	insert into Catelogue values (1001, 'philosophy', 1000)
+	insert into Catelogue values (1002, 'metaphysics', 1001)
+	insert into Catelogue values (1003, 'confucianism', 1001)
+	insert into Catelogue values (1004, 'mencius', 1003)
+	insert into Catelogue values (1005, 'literature', 1000)
+	insert into Catelogue values (1006, 'lin yutang', 1005)
+	insert into Catelogue values (1007, 'software', null)
+	insert into Catelogue values (1008, 'utilities', 1007)
+	insert into Catelogue values (1009, 'file management', 1008)
 end
 go
 
 -- query the n-level category product recursively
-with n(catelogueid, cateloguename, parentcatelogueid, cateloguelevel, concatenador) as 
+with n(CatelogueID, CatelogueName, ParentcatelogueID, CatelogueLevel, Concatenador) as 
 (
-    select catelogueid, cateloguename, parentcatelogueid, 1 as cateloguelevel, convert(varchar(max), cateloguename) as concatenador
-    from catelogue
-    where parentcatelogueid is null
+    select CatelogueID, CatelogueName, ParentcatelogueID, 1 as CatelogueLevel, convert(varchar(max), CatelogueName) as Concatenador
+    from Catelogue
+    where ParentcatelogueID is null
         union all
-    select m.catelogueid, m.cateloguename, m.parentcatelogueid, cateloguelevel+1, n.concatenador+' - '+convert(varchar(max), m.cateloguename) as concatenador
-    from catelogue as m, n
-    where n.catelogueid = m.parentcatelogueid
+    select m.CatelogueID, m.CatelogueName, m.ParentcatelogueID, CatelogueLevel+1, n.Concatenador+' - '+convert(varchar(max), m.CatelogueName) as Concatenador
+    from Catelogue as m, n
+    where n.CatelogueID = m.ParentcatelogueID
 )
-select concatenador as 'category products' from n order by concatenador asc 
+select Concatenador as 'Category Products' from n order by Concatenador asc 
