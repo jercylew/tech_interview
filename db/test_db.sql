883c7ad6e0038c2292e07b2900dee8390853281d
Jercy Liu <jercy_lew@yahoo.com>
2018-11-04 22:36:37 +1100

    Enhance the database design and test report

--- db/test_db.sql
+++ db/test_db.sql
@@ -5,18 +5,22 @@ if not exists (
 		select * from Catelogue
 )
 begin
-	insert into Catelogue values (1000, 'books', null)
-	insert into Catelogue values (1001, 'philosophy', 1000)
-	insert into Catelogue values (1002, 'metaphysics', 1001)
-	insert into Catelogue values (1003, 'confucianism', 1001)
-	insert into Catelogue values (1004, 'mencius', 1003)
-	insert into Catelogue values (1005, 'literature', 1000)
-	insert into Catelogue values (1006, 'lin yutang', 1005)
-	insert into Catelogue values (1007, 'software', null)
-	insert into Catelogue values (1008, 'utilities', 1007)
-	insert into Catelogue values (1009, 'file management', 1008)
+	insert into Catelogue values (1000, 'Books', null)
+	insert into Catelogue values (1001, 'Philosophy', 1000)
+	insert into Catelogue values (1003, 'Confucianism', 1001)
+	insert into Catelogue values (1005, 'Literature', 1000)
+	insert into Catelogue values (1007, 'Software', null)
+	insert into Catelogue values (1008, 'Utilities', 1007)
+	
+	insert into Product values (1002, 'Metaphysics', 25.50, 'A introduction to metaphysics', 1001)
+	insert into Product values (1004, 'Mencius', 35.20, 'A introduction to mencius', 1003)
+	insert into Product values (1006, 'Lin Yutang', 65.50, 'A introduction to lin yutang', 1005)
+	insert into Product values (1009, 'File Management', 125.50, 'A introduction to file management', 1008)
 end
 go
+
+select * from Product
+select * from Catelogue
 
 -- query the n-level category product recursively
 with n(CatelogueID, CatelogueName, ParentcatelogueID, CatelogueLevel, Concatenador) as 
@@ -29,4 +33,4 @@ with n(CatelogueID, CatelogueName, ParentcatelogueID, CatelogueLevel, Concatenad
     from Catelogue as m, n
     where n.CatelogueID = m.ParentcatelogueID
 )
-select Concatenador as 'Category Products' from n order by Concatenador asc 
+select  n.Concatenador + ' - ' + Product.ProductName from Product, n where Product.CatelogueID = n.CatelogueID order by n.Concatenador asc
